d6827f5b7021b89f8528be09680cc2c3c5fd193d
Kirill <kirill1991okt@gmail.com>
2023-03-10 23:14:44 +0300

    feat: implement quiz

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import './index.scss';
 
 const questions = [
@@ -8,7 +9,11 @@ const questions = [
   },
   {
     title: 'Компонент - это ... ',
-    variants: ['приложение', 'часть приложения или страницы', 'то, что я не знаю что такое'],
+    variants: [
+      'приложение',
+      'часть приложения или страницы',
+      'то, что я не знаю что такое',
+    ],
     correct: 1,
   },
   {
@@ -22,37 +27,68 @@ const questions = [
   },
 ];
 
-function Result() {
+function Result({ correctAnswer }) {
   return (
     <div className="result">
       <img src="https://cdn-icons-png.flaticon.com/512/2278/2278992.png" />
-      <h2>Вы отгадали 3 ответа из 10</h2>
-      <button>Попробовать снова</button>
+      <h2>
+        Вы отгадали {correctAnswer} ответа из {questions.length}
+      </h2>
+      <a href="/">
+        <button>Попробовать снова</button>
+      </a>
     </div>
   );
 }
 
-function Game() {
+function Game({ question, progress, onClickAnswer }) {
   return (
     <>
       <div className="progress">
-        <div style={{ width: '50%' }} className="progress__inner"></div>
+        <div
+          style={{ width: `${progress}%` }}
+          className="progress__inner"
+        ></div>
       </div>
-      <h1>Что такое useState?</h1>
+      <h1>{question.title}</h1>
       <ul>
-        <li>Это функция для хранения данных компонента</li>
-        <li>Это глобальный стейт</li>
-        <li>Это когда на ты никому не нужен</li>
+        {question.variants.map((text, i) => (
+          <li key={text} onClick={() => onClickAnswer(i)}>
+            {text}
+          </li>
+        ))}
       </ul>
     </>
   );
 }
 
 function App() {
+  const [step, setStep] = useState(0);
+  const [correctAnswer, setCorrectAnswer] = useState(0);
+
+  const question = questions[step];
+
+  const progress = Math.round((step / questions.length) * 100);
+
+  const onClickAnswer = (index) => {
+    setStep(step + 1);
+
+    if (question.correct === index) {
+      setCorrectAnswer(correctAnswer + 1);
+    }
+  };
+
   return (
     <div className="App">
-      <Game />
-      {/* <Result /> */}
+      {step !== questions.length ? (
+        <Game
+          question={question}
+          progress={progress}
+          onClickAnswer={onClickAnswer}
+        />
+      ) : (
+        <Result correctAnswer={correctAnswer} />
+      )}
     </div>
   );
 }
