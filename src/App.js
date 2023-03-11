21986ce9389cda44c7a0cb03c19a5ce58643cd09
Kirill <kirill1991okt@gmail.com>
2023-03-11 21:44:37 +0300

    feat: implement users

--- src/App.js
+++ src/App.js
@@ -6,10 +6,47 @@ import { Users } from './components/Users';
 // Тут список пользователей: https://reqres.in/api/users
 
 function App() {
+  const [users, setUsers] = React.useState([]);
+  const [invited, setInvited] = React.useState([]);
+  const [isLoading, setIsLoading] = React.useState(true);
+  const [stateInvited, setStateInvited] = React.useState(true);
+
+  const onClickToInvited = (id) => {
+    if (invited.includes(id)) {
+      setInvited((prev) => prev.filter((numInArr) => numInArr !== id));
+    } else {
+      setInvited((prev) => [...prev, id]);
+    }
+  };
+
+  const onSendInvite = () => {
+    setStateInvited(false);
+  };
+
+  React.useEffect(() => {
+    fetch('https://reqres.in/api/users')
+      .then((res) => res.json())
+      .then((json) => setUsers(json.data))
+      .catch((err) => {
+        console.log(err);
+        alert(err);
+      })
+      .finally(() => setIsLoading(false));
+  }, []);
+
   return (
     <div className="App">
-      <Users />
-      {/* <Success /> */}
+      {stateInvited ? (
+        <Users
+          items={users}
+          isLoading={isLoading}
+          invited={invited}
+          onClickToInvited={onClickToInvited}
+          onSendInvite={onSendInvite}
+        />
+      ) : (
+        <Success count={invited.length} />
+      )}
     </div>
   );
 }
--- src/components/Users/index.jsx
+++ src/components/Users/index.jsx
@@ -2,14 +2,23 @@ import React from 'react';
 import { Skeleton } from './Skeleton';
 import { User } from './User';
 
-export const Users = ({ items, isLoading }) => {
+export const Users = ({ items, isLoading, onClickToInvited, invited, onSendInvite}) => {
+
+  const [inputValue, setInputValue] = React.useState('');
+
+
+  const onSetInputValue = (event) => {
+    setInputValue(event.target.value);
+  }
+
+
   return (
     <>
       <div className="search">
         <svg viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
           <path d="M12.9 14.32a8 8 0 1 1 1.41-1.41l5.35 5.33-1.42 1.42-5.33-5.34zM8 14A6 6 0 1 0 8 2a6 6 0 0 0 0 12z" />
         </svg>
-        <input type="text" placeholder="Найти пользователя..." />
+        <input value={inputValue} onChange={(e) => onSetInputValue(e)} type="text" placeholder="Найти пользователя..." />
       </div>
       {isLoading ? (
         <div className="skeleton-list">
@@ -19,10 +28,15 @@ export const Users = ({ items, isLoading }) => {
         </div>
       ) : (
         <ul className="users-list">
-          <User />
+          {items.filter(item => {
+            const fullName = (item.first_name + item.last_name).toLowerCase();
+            return fullName.includes(inputValue.toLowerCase()) || item.email.includes(inputValue.toLowerCase());
+          }).map(item => (
+            <User {...item} key={item.id} isInvited={invited.includes(item.id)} onClickToInvited={onClickToInvited}/>
+          ))}
         </ul>
       )}
-      <button className="send-invite-btn">Отправить приглашение</button>
+      <button onClick={onSendInvite} className="send-invite-btn" disabled={invited.length ? false : true}>Отправить приглашение</button>
     </>
   );
 };
